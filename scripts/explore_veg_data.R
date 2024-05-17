704541361250160fd1a9fc3b619df47230b035fe
Collins <Courtney.Collins@gov.bc.ca>
2024-05-16 17:27:36 -0700

    pull in new plot filtering table from @whmacken

--- scripts/explore_veg_data.R
+++ scripts/explore_veg_data.R
@@ -26,45 +26,80 @@ rm(my_points)
 rm(veg_dat)
 gc()
 
-sort(names(all_dat))
-unique(all_dat$BECSiteUnit)
-
 #look at data across species/sites 
-all_dat<-filter(all_dat, !is.na(BECSiteUnit))
+#match with updated site series info from Will 5/16/24
+ss_cleaned<-read.csv('data/updated_siteseries.csv')
+all_dat<-left_join(all_dat, ss_cleaned) 
 
-spp_tab<-all_dat%>%
-                group_by(Species, BECSiteUnit, GIS_BGC, SiteSeries, UserSiteUnit)%>%
+#remove anything not designated A BEC or BGC Unit from Will's list and that is not tree layer (TotalA)
+tree_dat<-filter(all_dat, !is.na(bgc) &!is.na(TotalA))
+tree_dat<-separate(tree_dat, SiteUnit, into = c("Zone", "Site"), sep = '/', remove=F) #ignore warning 
+sort(unique(tree_dat$Species))
+
+#remove anything not assigned to spp level 
+#tree_dat<-subset(tree_dat, Species!="SALIX" & Species!="TAXUS"&Species!="MALUS"&Species!="UNKNOWN"&Species!="PICEA"&
+#     Species!="POPULUS"& Species!="BETULA" & Species!="ALNUS"  & Species!="TSUGA" & Species!="THUJA")
+  
+spp_tab0<-tree_dat%>%
+  group_by(Species)%>%
+  summarise(nobs=n())
+spp_keep<-subset(spp_tab0, nobs>100)
+spp_keep<-spp_keep$Species #30 total 
+
+#remove any tree spp with <100 total obs  
+tree_dat<-filter(tree_dat, Species %in% spp_keep) 
+
+spp_tab<-tree_dat%>%
+                group_by(Species,SiteUnit, bgc,Site)%>%
                 summarise(nobs=n())
 
-#subset to most measured spp- Western Hemlock
-Hw<-subset(all_dat, Species=='TSUGHET') 
-#plot across different sites 
-#Site series are the different sites wet to dry (101-Zonal)- Needs cleaning/renumbering 
-str(Hw)
-
-ggplot(Hw, aes(y=TotalA, x=MAT, color=SiteSeries))+
-  geom_point()+
-  facet_wrap(~GIS_BGC)+
-  geom_smooth(method='lm', se = F)+
-  #theme(legend.position = 'none')
-  xlim(-2.5, 12) #git rid of outliers 
-
-#Doug Fir
-Fd<-subset(all_dat, Species=='PSEUMEN1')
-ggplot(Fd, aes(y=TotalA, x=MAT, color=SiteSeries))+
-  geom_point()+
-  facet_wrap(~GIS_BGC)+
-  geom_smooth(method='lm', se = F)+
-  #theme(legend.position = 'none')
-  xlim(-2.5, 12) #git rid of outliers 
-
+#look at most measured spp- 
 #Western red Cedar 
-Cw<-subset(all_dat, Species=='THUJPLI')
-ggplot(Cw, aes(y=TotalA, x=MAT, color=SiteSeries))+
+Cw<-subset(tree_dat, Species=='THUJPLI')
+ggplot(Cw, aes(y=TotalA, x=MAT, color=Site))+
   geom_point()+
-  facet_wrap(~GIS_BGC)+
-  geom_smooth(method='lm', se = F)+
+  facet_wrap(~bgc) #+
+  #geom_smooth(method='lm', se = F)#+
   #theme(legend.position = 'none')
-  xlim(-2.5, 12) #git rid of outliers 
+  #xlim(-2.5, 12) #git rid of outliers 
 
-#very uneven sampling across BGCs, need some threshold for number of siteseries per BGC etc, also probably number of records per Site Series
+#Western Hemlock
+Hw<-subset(tree_dat, Species=='TSUGHET') 
+ggplot(Hw, aes(y=TotalA, x=MAT, color=Site))+
+  geom_point()+
+  facet_wrap(~bgc)
+  #geom_smooth(method='lm', se = F)#+
+  #theme(legend.position = 'none')
+ # xlim(-2.5, 12) #git rid of outliers 
+
+
+#uneven sampling across BGCs, need some threshold for number of siteseries per BGC etc, also probably number of records per Site Series
+all_dat<-filter(all_dat, !is.na(BECSiteUnit))
+
+
+#add feasibility tables in??
+
+names(all_dat)
+
+#test model
+library(lme4)
+library(lmerTest)
+library(dplyr)
+hist(Hw$TotalA)
+
+hist(log(Hw$TotalA))
+hist(Hw$MAT)
+
+unique(Hw$MAT)
+
+#categorize MAT            
+Hw<-mutate(Hw, MATcat=cut(MAT, breaks = 5,
+      labels = c("MAT1", "MAT2", "MAT3", "MAT4", "MAT5")))
+
+
+Hwmod<-lmer(log(TotalA)~ scale(MAT) + scale(PPT) + (MATcat|bgc:Site),  Hw) #can't do nested Random slope 
+summary(Hwmod)
+
+Hwmod<-lmer(log(TotalA)~ scale(MAT) + scale(PPT) + (MATcat|GIS_BGC) + (MATcat|GIS_BGC:SiteSeries),  Hw) #can't do nested RE
+
+#climate, SNR, aSMR, aspect, slope 
