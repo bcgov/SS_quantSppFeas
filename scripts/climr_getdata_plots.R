bec39415ada0cca9c6a2480f9bac06b8e60475d0
ewhmacken <you@example.com>
2024-10-18 12:43:39 -0700

    writing to file for retrieval in subsequent scripts

--- scripts/climr_getdata_plots.R
+++ scripts/climr_getdata_plots.R
@@ -59,7 +59,7 @@ addVars(clim_dat)
 #sanity check
 #merge back with plot data 
 plot_dat<-left_join(plot_dat, rename(clim_dat, PlotNumber=id))
-
+fwrite(plot_dat, "data/plot_dat_climr.csv")
 ggplot(plot_dat, aes(x=Elevation, y=MAT))+
   geom_point()+
   geom_smooth(method='lm')+
@@ -74,4 +74,9 @@ ggplot(plot_dat, aes(x=Elevation, y=PPT))+
   xlim(0,4000) #git rid of outliers 
 #precip more interesting - is this picking up snow??
 
-
+ggplot(plot_dat, aes(x=Elevation, y=CMD))+
+  geom_point()+
+  geom_smooth()+
+  #ylim(-10, 10) + 
+  xlim(0,4000) #git rid of outliers 
+#precip more interesting - is this picking up snow??
