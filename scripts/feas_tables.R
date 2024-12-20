1b72a4f7da8bc10ec08e8308a7201ad42a220fd2
Courtney Collins <Courtney.Collins@gov.bc.ca>
2024-12-19 18:24:32 -0800

    scripts and data for bayesian feasibility modeling workflow

--- scripts/feas_tables.R
+++ scripts/feas_tables.R
@@ -12,12 +12,14 @@
 #This script does the following:
 #pulls in cleaned feasibility tables w/ climate data created from 0_DataPrep_OrdinalForests.Rmd in Tree Feasibility Prediction repr @whmacken 
 #filters BGCs further
-#runs PCA on climate vars and merges with feas values 
-#merges feas values with BEC plot data (tree abundance)
+#runs PCA on climate vars and merges with feas values (sourced)
+#merges feas values with BC BEC plot data (tree abundance)
 #calculates and plots mean abundances by feas rating for top spp 
 
+library(tidyverse)
+
 #load feasibility data with site & climate info---- 
-feas.dat <- readRDS("C:/Users/ccollins/OneDrive - Government of BC/WNA_BGC/OrdinalForest_data.rds")
+feas.dat <- readRDS("C:/Users/ccollins/OneDrive - Government of BC/CCISS/ccissv13_workingfiles/Feasibility_modelling/OrdinalForest_data.rds")
 
 #filter out small BGCs not included in training/projections (from BGC projections repo- create training set.R)
 BGC_exclude<-c( "BWBScm"  ,"CMAun_OR",  "CMAwh",   "CWHxs",  "ESSFdcp", "ESSFdh1" ,"ESSFdh2"  ,"ESSFdh_WA","ESSFwh_MT","ESSFxcp", "ESSFxcw",  
@@ -29,9 +31,8 @@ feas.dat<-na.omit(feas.dat)
 
 unique(feas.dat$BGC)#309-> 366 in projections, missing 57 subzones 
 unique(feas.dat$spp)
-unique(tree_dat$Species)
 
-#select climate variables from BGC model
+##select climate variables from BGC model
 climrVars = c("CMD_sm", "DDsub0_sp", "DD5_sp", "Eref_sm", "Eref_sp", "EXT", 
               "MWMT", "NFFD_sm", "NFFD_sp", "PAS", "PAS_sp", "SHM", "Tave_sm", 
               "Tave_sp", "Tmax_sm", "Tmax_sp", "Tmin", "Tmin_at", "Tmin_sm", 
@@ -39,58 +40,25 @@ climrVars = c("CMD_sm", "DDsub0_sp", "DD5_sp", "Eref_sm", "Eref_sp", "EXT",
 names(feas.dat)
 
 feas.dat<-mutate(feas.dat, CMD.total=CMD.def +CMD)
-varsl<-c(c("zone", "BGC", "SS_NoSpace","spp", "newfeas",  "WNA_DEM_4326_clipped" ,"xcoord", "ycoord") , climrVars)
+varsl<-c(c("zone", "BGC", "SS_NoSpace","spp", "newfeas",  "fid", "WNA_DEM_4326_clipped" ,"xcoord", "ycoord") , climrVars)
 feas.dat.sub<-select(feas.dat, varsl)
 
+save(feas.dat.sub, file="data/feasibility_data.Rdata")
+
 #calculate PC axes for climate params----
-library(vegan)
-library(ape)
+source("scripts/climPCAs.R")
 
-climdat<- select(feas.dat.sub, climrVars) %>%
-  #mutate(PASlog=log(PAS), PAS_splog=log(PAS_sp),PPT_MJlog=log(PPT_MJ), PPT_JASlog=log(PPT_JAS))%>% #log precip vars
-  #select(-PAS, -PAS_sp, -PPT_MJ, -PPT_JAS) %>%
-  scale(.) # standardize the data
-
-# Perform principal component analysis (PCA) on the standardized data
-climpca<-  prcomp(climdat)
-
-# Print a summary of the PCA results
-summary(climpca)
-biplot(climpca)
-
-# Convert the PCA results to a tibble and add a column for observation names
-climpca_df <- as_tibble(climpca$x) %>% 
-  mutate(observation = rownames(climpca$x))%>%select(PC1, PC2, PC3, observation)
-
-#in vegan
-climpca2 <- vegan::rda(climdat, scale=F) #already scaled above 
-scores <- scores(climpca)
-biplot(climpca) 
-eigenvals(climpca2)
-#loading scores of climate params onto PCA axes 
-climloads<-envfit(climpca, climdat, permutations = 999, strata = NULL)
-climloads
-scrs <- as.data.frame(scores(climloads, display = "vectors"))
-scrs<-add_rownames(scrs, "clim")
-
-ggplot(scrs) +
-  geom_point(mapping = aes(x = PC1, y = PC2)) +
-  coord_fixed() + ## need aspect ratio of 1!
-  geom_segment(data = scrs,
-               aes(x = 0, xend = PC1, y = 0, yend = PC2),
-               arrow = arrow(length = unit(0.25, "cm")), colour = "grey") +
-  geom_text(data = scrs, aes(x = PC1, y = PC2, label = clim),
-            size = 3)
-
-#merge PCs back with feas data---- 
-feas.dat.sub<-add_rownames(feas.dat.sub, "observation")
-feas.dat.sub<-left_join(climpca_df, feas.dat.sub)
-
+#save again
 save(feas.dat.sub, file="data/feasibility_data.Rdata")
 
 
 #merge feas & tree data---- 
-feas.dat.sub<-mutate(feas.dat.sub, Species= case_when(spp=="Ba"~"ABIEAMA",
+#deal with duplicates for coastal and interior spp 
+load(file="data/feasibility_data.Rdata")
+feas.dat.sub<-subset(feas.dat.sub, spp!="Plc"& spp!="Pli"& spp!="Fdc" & spp!="Fdi"& spp!="Pyc"& spp!="Pyi")
+
+#add full spp codes to feas df 
+feas.dat.subx<-mutate(feas.dat.sub, Species= case_when(spp=="Ba"~"ABIEAMA",
                                                       spp=="Ra" ~"ARBUMEN",
                                                       spp=="Bg"~"ABIEGRA", 
                                                       spp=="Bl"~"ABIELAS", 
@@ -107,134 +75,176 @@ feas.dat.sub<-mutate(feas.dat.sub, Species= case_when(spp=="Ba"~"ABIEAMA",
                                                       spp=="Sxl"~"PICEXLU",
                                                       spp=="Pa"~"PINUALB",
                                                       spp=="Pl"~"PINUCON",
-                                                      spp=="Plc"~"PINUCON", #coastal
-                                                      spp=="Pli"~"PINUCON", #interior
-                                                      spp=="Pyc"~"PINUPON",
-                                                      spp=="Pyi"~"PINUPON",
+                                                     # spp=="Plc"~"PINUCON", #coastal
+                                                    #  spp=="Pli"~"PINUCON", #interior
+                                                    #  spp=="Pyc"~"PINUPON",
+                                                    #  spp=="Pyi"~"PINUPON",
+                                                      spp=="Py"~"PINUPON",
                                                       spp=="Pw"~"PINUMON",
                                                       spp=="Acb"~"POPUBAL",
                                                       spp=="At"~"POPUTRE",
                                                       spp=="Act"~"POPUTRI",
                                                       spp=="Fd"~"PSEUMEN", 
-                                                      spp=="Fdi"~"PSEUMEN", #interior
-                                                      spp=="Fdc"~"PSEUMEN", #coastal
+                                                     # spp=="Fdi"~"PSEUMEN", 
+                                                     # spp=="Fdc"~"PSEUMEN", 
                                                       spp=="Tw"~ "TAXUBRE",
                                                       spp=="Cw"~ "THUJPLI",
                                                       spp=="Hw"~ "TSUGHET",
                                                       spp=="Hm"~"TSUGMER")) 
-tree_dat_sub<-select(tree_dat, PlotNumber, Species, TotalA, SiteUnit, bgc,NutrientRegime_clean,MoistureRegime_clean, Latitude, Longitude)%>%rename(BGC=bgc)
 
-tree_dat_sub<-mutate(tree_dat_sub,  SS_NoSpace= gsub(" ", "", SiteUnit)) #create matching column to feas table
 
-names(feas.dat.sub)
-feas.dat.sub<-left_join(feas.dat.sub, tree_dat_sub)  
-#check merge worked ok 
-plot(feas.dat.sub$ycoord~feas.dat.sub$Latitude)??
-  
-avgs<-group_by(feas.dat.sub, zone, BGC, SS_NoSpace, Species, spp, newfeas_ord)%>%summarise(mean_abund_ss=mean(TotalA, na.rm = T))%>%ungroup(.)%>%
-group_by(BGC)%>% mutate(mean_abund_subzone=mean(mean_abund_ss, na.rm = T))%>% ungroup(.)%>%
-group_by(zone)%>% mutate(mean_abund_zone=mean(mean_abund_subzone, na.rm = T))#4 is actually a 5 here (i.e. zero)
+#remove climate data for now 
+#varsl<-c("zone", "BGC", "SS_NoSpace","spp", "Species", "newfeas",  "fid", "WNA_DEM_4326_clipped" ,"xcoord", "ycoord") 
+#feas.dat.subx<-select(feas.dat.subx, varsl)
 
+#take out the US and alberta stuff because it won't match plot data
+feas.dat.subx<-filter(feas.dat.subx, !grepl('_CA|_OR|_WA|_ID|_MT|_CA|_WY|_CO|_NV|UT|BSJP|abE|abN|abS|abE|abC|SBAP|SASbo|PPxh|MSd|MSx', SS_NoSpace))
+
+#load BC tree data 
+load(file="data/tree_data_cleaned_wzeros.Rdata")  
+#load(file="data/tree_data_cleaned.Rdata")
+tree_dat<-tree_dat_wzeros
+rm(tree_dat_wzeros)
+gc()
+
+#update site series naming plot data
+ss <- read.csv("C:/Users/ccollins/OneDrive - Government of BC/CCISS/ccissv13_workingfiles/Feasibility_modelling/All_BGC12DEC2024_SU.csv")
+ss<-mutate(ss, bgc= gsub(" ", "", bgc))
+tree_dat<-rename(tree_dat, SiteUnitold=SiteUnit)
+tree_dat<-left_join(tree_dat, ss)
+tree_dat<-mutate(tree_dat, ss_new= if_else(SiteUnitold==SiteUnit, 'N', 'Y'))
+
+#subset cols of interest 
+tree_dat_sub<-select(tree_dat, PlotNumber, Species, TotalA, SiteUnitold, ss_new,
+                     SiteUnit, bgc,  NutrientRegime_clean,MoistureRegime_clean, Latitude, Longitude)
+
+#create matching columns to feas tables
+tree_dat_sub<-mutate(tree_dat_sub,  SS_NoSpace= gsub(" ", "", SiteUnit))%>%rename(BGC=bgc)
+#names(feas.dat.sub)
+#names(tree_dat_sub)
+#ssfeas<-unique(feas.dat.sub$SS_NoSpace)
+#sstree<-unique(tree_dat_sub$SS_NoSpace)
+#ssmatches<-subset(tree_dat_sub, SS_NoSpace %in% ssfeas)
+#ssmatches2<-subset(feas.dat.sub, SS_NoSpace %in% sstree)
+
+#join with BC data
+feas.dat.suby<-left_join(feas.dat.subx, tree_dat_sub)  
+feas.dat.suby<-subset(feas.dat.suby,!is.na(TotalA))
+
+#create list with missing feas ratings for imputing
+feas.dat.subz<-left_join(tree_dat_sub, feas.dat.subx)  
+
+#set feas as ord factor
+feas.dat.suby$newfeas_ord<-ordered(feas.dat.suby$newfeas, levels = c(4,3, 2, 1))
+str(feas.dat.suby$newfeas_ord)
+knitr::kable(group_by(feas.dat.suby, newfeas_ord)%>%summarise(counts=n())) #4 is actually a 5 here (i.e. zero)
+
+#other plot data-right now not at SS level so can't merge with feas tables #### 12/2024
+#load US tree data 
+#load( file= )
+#names(feas.dat.sub)
+#cols<-names(feas.dat.sub)
+#all_US_dat<-select(all_US_dat)
+#all_US_dat<-subset(all_US_dat, select = names(all_US_dat) %in% cols) 
+
+#load AB tree data 
+
+#save feas plus plot data
+feas.dat.sub<-feas.dat.suby
+
+save(feas.dat.sub, file="data/feasibility_abundance_data.Rdata")
+
+
+#plot by spp with plot data 
+#calculate average abundances by feas scores
+avgs<-group_by(feas.dat.sub, zone, BGC, SS_NoSpace, Species, spp, newfeas_ord)%>%summarise(mean_abund_ss=mean(TotalA, na.rm = T), sd_abund_ss=sd(TotalA, na.rm = T))%>%ungroup(.)%>%
+  group_by(BGC)%>% mutate(mean_abund_subzone=mean(mean_abund_ss, na.rm = T))%>% ungroup(.)%>%
+  group_by(zone)%>% mutate(mean_abund_zone=mean(mean_abund_subzone, na.rm = T))#4 is actually a 5 here (i.e. zero)
 avgs<-na.omit(avgs)
 
-
-
-#plot by spp with data 
+#how many spp have >1 rating?
 check<-group_by(feas.dat.sub, spp)%>%summarise(n_ratings= n_distinct(newfeas), abund=mean(TotalA, na.rm=T))
 check<-subset(check, n_ratings>1 & !is.na(abund))
 check$spp
 
+#spp plots 
 #At- 1 vs 2 not clear in IDF
 ggplot(subset(avgs, Species=="POPUTRE"), aes(x = newfeas_ord, y = log(mean_abund_ss+1), colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("At")
 #Ba- fine, rare
 ggplot(subset(avgs, Species=="ABIEAMA"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Ba")
 #Bl-looks good
 ggplot(subset(avgs, Species=="ABIELAS"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
-#Cw- looks good 
-ggplot(subset(avgs, Species=="TSUGHET"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')+ ggtitle("Bl")
+#Cw- looks good - 2 vs 3 not different in CWH
+ggplot(subset(avgs, Species=="THUJPLI"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Cw")
 #Ep- looks good 
 ggplot(subset(avgs, Species=="BETUPAP"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
-#Fd - 4s too high in most zones - driven by Fdi & Fdc entries 
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')+ggtitle("Ep")
+#Fd - fine 
 ggplot(subset(avgs, Species=="PSEUMEN"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
-ggplot(subset(avgs, Species=="PSEUMEN"& spp=="Fd"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
-  geom_point(position=position_jitterdodge(dodge.width=0.9)) +
-  geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
-ggplot(subset(avgs, spp=="Fdi"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
-  geom_point(position=position_jitterdodge(dodge.width=0.9)) +
-  geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
-ggplot(subset(avgs, spp=="Fdc"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
-  geom_point(position=position_jitterdodge(dodge.width=0.9)) +
-  geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Fd")
 #Hm- fine, rare 
 ggplot(subset(avgs, Species=="TSUGMER"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
-#Hw- looks good 
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Hm")
+#Hw- looks good - CWH 2 is higher than 1 
 ggplot(subset(avgs, Species=="TSUGHET"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Hw")
 #Lw-looks good 
 ggplot(subset(avgs, Species=="LARIOCC"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
-#Pl- 4s too high in most zones- driven by Plc and Pli entries... 
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Lw")
+#Pl- fine
 ggplot(subset(avgs, Species=="PINUCON"), aes(x = newfeas_ord, y = mean_abund_ss, alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
-#Plc, Plis have high abundances for 4s 
-ggplot(subset(avgs, spp=="Plc"), aes(x = newfeas_ord, y = mean_abund_ss, alpha=0.5))+
-  geom_point(position=position_jitterdodge(dodge.width=0.9)) +
-  geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() #+ theme(legend.position='none')
-ggplot(subset(avgs, spp=="Pli"), aes(x = newfeas_ord, y = mean_abund_ss, alpha=0.5))+
-  geom_point(position=position_jitterdodge(dodge.width=0.9)) +
-  geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() #+ theme(legend.position='none')
-#remove them 
-ggplot(subset(avgs, Species=="PINUCON"& spp=="Pl"), aes(x = newfeas_ord, y = mean_abund_ss, alpha=0.5))+
-  geom_point(position=position_jitterdodge(dodge.width=0.9)) +
-  geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Pl")
 #Ss- looks good 
 ggplot(subset(avgs, Species=="PICEMAR"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
-#Ss- high values for 4 in CWH
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Se")
+#Ss- 2 higher than 1 in SBS
 ggplot(subset(avgs, Species=="PICESIT"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
-#Yc - 1 & 2 almost the same in CWH
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Ss")
+#issues that PICENE all rated 4s 
+ggplot(subset(avgs, Species=="PICEENE"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
+  geom_point(position=position_jitterdodge(dodge.width=0.9)) +
+  geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Se")
+#Yc -
 ggplot(subset(avgs, Species=="CALLNOO"), aes(x = newfeas_ord, y = mean_abund_ss, colour="blue", alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none')
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Yc")
+
+#look at 4s with high abundances
+check<-subset(avgs, newfeas_ord=="4" & mean_abund_ss>5)
+unique(check$spp)#almost all spp 
+
+
+
 
 ##OLD###
 #filter feas table
@@ -246,7 +256,5 @@ spp_keep<-subset(spp_tab0, nobs>300)
 feas_tab<-subset(feas_tab, Species %in% spp_keep$Species)
 rm(spp_tab0)
 
-
-spp_keep$Species
 
 
