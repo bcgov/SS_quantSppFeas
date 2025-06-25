001d4bc456a86282a4bf80df26ffa45e0a372263
Courtney Collins <courtneygrace125@gmail.com>
2025-06-25 14:51:04 -0700

    clean up

--- scripts/explore_veg_data.R
+++ scripts/explore_veg_data.R
@@ -30,18 +30,11 @@ rm(site_dat)
 rm(veg_dat)
 gc()
 
-#look at data across species/sites 
-#match with updated site series info from Will - awaiting final BEC v13 x plot numbers list
-ss_cleaned<-read.csv("C:/Users/ccollins/OneDrive - Government of BC/CCISS/ccissv13_workingfiles/Feasibility_modelling/All_BGC12DEC2024_SU.csv") 
-all_dat<-left_join(all_dat, ss_cleaned) 
-
 #remove anything that is not tree layer (TotalA or B)
 #how to deal with B layer?? add to A layer? if nothing in A layer, use B layer? average? 
 tree_dat<-filter(all_dat, !is.na(TotalA)|!is.na(TotalB))  
-#(not designated A BGC Unit from Will's list) and #!is.na(bgc))
 rm(all_dat)
 gc()
-tree_dat<-separate(tree_dat, SiteUnit, into = c("Zone", "Site"), sep = '/', remove=F) #can ignore warning 
 sort(unique(tree_dat$Species))
 
 spp_tab0<-tree_dat%>%
@@ -99,14 +92,9 @@ tree_dat<-subset(tree_dat, SlopeGradient<101)
 unique(tree_dat$NutrientRegime)# need to finalize calls on all
 unique(tree_dat$MoistureRegime) # need to finalize calls on all 
 
-#try to create table to fill in from
-SMRs<-group_by(tree_dat, SiteUnit)%>%dplyr::select(MoistureRegime, NutrientRegime) %>%distinct()%>%mutate(ctMoist=n_distinct(MoistureRegime))%>%
-  mutate(ctNut=n_distinct(NutrientRegime))%>%mutate(MoistNA= is.na(MoistureRegime))%>%mutate(NutNA= is.na(NutrientRegime))
 
 #set rules for transitional SMRs and SNRs 
-sort(table(SMRs$NutrientRegime)) #table-base R
-                                
-SMRs<-mutate(SMRs, NutrientRegime_clean = case_when(NutrientRegime=="A" ~ "A",
+tree_dat<-mutate(tree_dat, NutrientRegime_clean = case_when(NutrientRegime=="A" ~ "A",
                                       NutrientRegime=="A+" ~ "A",
                                       NutrientRegime=="AB" ~ "B",
                                       NutrientRegime=="B" ~ "B",
@@ -135,8 +123,7 @@ SMRs<-mutate(SMRs, NutrientRegime_clean = case_when(NutrientRegime=="A" ~ "A",
                                       NutrientRegime=="3" ~ "NA",
                                       NutrientRegime=="6" ~ "NA"))
 
-sort(table(SMRs$MoistureRegime))
-SMRs<-mutate(SMRs, MoistureRegime_clean = case_when(MoistureRegime=="$" ~ "NA",
+tree_dat<-mutate(tree_dat, MoistureRegime_clean = case_when(MoistureRegime=="$" ~ "NA",
                                                     MoistureRegime=="0" ~ "0",
                                                     MoistureRegime=="0-1" ~ "1",
                                                     MoistureRegime=="0+" ~ "0",
@@ -181,15 +168,12 @@ SMRs<-mutate(SMRs, MoistureRegime_clean = case_when(MoistureRegime=="$" ~ "NA",
                                                     MoistureRegime=="8" ~ "8",
                                                     MoistureRegime=="PM" ~ "NA"))
 
-SMRs<-dplyr::select(SMRs, SiteUnit, MoistureRegime, NutrientRegime, NutrientRegime_clean, MoistureRegime_clean)
-
 #merge back to tree data
-tree_dat<-left_join(tree_dat, SMRs)
 unique(tree_dat$MoistureRegime_clean)
 unique(tree_dat$NutrientRegime_clean)
 
 #remove rows with NAs in Moisture/Nutrient regimes 
-tree_dat<- subset(tree_dat, NutrientRegime_clean!="NA"& MoistureRegime_clean!="NA")
+tree_dat<- subset(tree_dat, NutrientRegime_clean!="NA"& MoistureRegime_clean!="NA") #2k rows 
 
 #look at other veg vars of interest
 hist(tree_dat$StrataCoverTree) #normally distributed-ish  #is this all tree spp combined??
@@ -198,8 +182,8 @@ hist(tree_dat$TotalA) #right skewed - beta dist?
 hist(log(tree_dat$TotalA)) #not bad... 
 
 #create a year column
-#library(lubridate)
-#tree_dat<-mutate(tree_dat,year=year(Date))
+library(lubridate)
+tree_dat<-mutate(tree_dat,year=year(Date))
 names(tree_dat)
 
 #fix species names 
@@ -208,177 +192,162 @@ tree_dat<-mutate(tree_dat, Species=if_else(Species=="PINUCON1"|Species=="PINUCON
 
 unique(tree_dat$Species)
 
-#check for NAs 
-tree_dat2<-na.omit(tree_dat)
-nas<-anti_join(tree_dat, tree_dat2)
-
-nacols <- function(df) {
-  colnames(df)[unlist(lapply(df, function(x) any(is.na(x))))]
-}
-nacols(nas)
-names(tree_dat)
-
 #save cleaned tree data----
 save(tree_dat, file="data/tree_data_cleaned.Rdata")
-rm(all_dat)
+rm(BEC_data)
+rm(clim_dat)
 gc()
 
 #add zeroes to plots where species not observed---- 
+#NOT CURRENTLY USING 6/2025
 #load(file="data/tree_data_cleaned.Rdata")
-spp_tab0<-tree_dat%>%
-  group_by(Species)%>%
-  summarise(nobs=n())
-spp_keep<-subset(spp_tab0, nobs>250)
-spp_keep<-spp_keep$Species #16 
+#spp_tab0<-tree_dat%>%
+#  group_by(Species)%>%
+#  summarise(nobs=n())
+#spp_keep<-subset(spp_tab0, nobs>250)
+#@spp_keep<-spp_keep$Species  
 
-#filter to 16
-tree_dat<-subset(tree_dat, Species %in% spp_keep)
-unique(tree_dat$Species)              
+#filter 
+#tree_dat<-subset(tree_dat, Species %in% spp_keep)
+#unique(tree_dat$Species)              
 
 #expand grid
-tree_dat_wzeros<-expand.grid(PlotNumber=unique(tree_dat$PlotNumber), Species=spp_keep)
+#tree_dat_wzeros<-expand.grid(PlotNumber=unique(tree_dat$PlotNumber), Species=spp_keep)
 
 #bring back in climate data by plot 
-load(file= 'data/clim_dat.plots.Rdata')
-tree_dat_wzeros<-left_join(tree_dat_wzeros, plot_dat)
-gc()
+#load(file= 'data/clim_dat.plots.Rdata')
+#tree_dat_wzeros<-left_join(tree_dat_wzeros, plot_dat)
+#gc()
 
 #merge back in plot data (minus climate)
-sort(names(tree_dat))
-tree_dat<-dplyr::select(tree_dat,  PlotNumber, Species, TotalA, ID, ProjectID, Date, #SiteSurveyor,PlotRepresenting, Location,
-  Longitude, Latitude, LocationAccuracy, SubZone, SiteSeries, MoistureRegime,           
-  NutrientRegime, Elevation, SlopeGradient, Aspect, MesoSlopePosition, #SubstrateDecWood, SubstrateBedRock,
+#sort(names(tree_dat))
+#tree_dat<-dplyr::select(tree_dat,  PlotNumber, Species, TotalA, TotalB, ID, ProjectID, Date, #SiteSurveyor,PlotRepresenting, Location,
+ # Longitude, Latitude, LocationAccuracy, SubZone, SiteSeries, MoistureRegime,           
+  #NutrientRegime, Elevation, SlopeGradient, Aspect, MesoSlopePosition, #SubstrateDecWood, SubstrateBedRock,
   #SubstrateRocks, SubstrateMineralSoil, SubstrateOrganicMatter, SubstrateWater, SurficialMaterialSurf, 
-  SoilDrainage, HumusForm, StrataCoverTree, StrataCoverShrub, StrataCoverHerb, StrataCoverMoss, UserSiteUnit, GIS_BGC, GIS_BGC_VER,
-  StrataCoverTotal, Elevation_overlay, SiteUnit, Site, bgc, NutrientRegime_clean, MoistureRegime_clean, year)
+  #SoilDrainage, HumusForm, StrataCoverTree, StrataCoverShrub, StrataCoverHerb, StrataCoverMoss, UserSiteUnit, GIS_BGC, GIS_BGC_VER,
+  #StrataCoverTotal, Elevation_overlay, NutrientRegime_clean, MoistureRegime_clean, year)
+
 #pull out tree cover 
-plot_dat2<-dplyr::select(tree_dat, -Species, -TotalA, -ID)%>%distinct(.)
-tree_dat_wzeros<-left_join(tree_dat_wzeros, plot_dat2) #check warnings about duplicated info 
-tree_dat_wzeros<-distinct(tree_dat_wzeros)
+#plot_dat2<-dplyr::select(tree_dat, -Species, -TotalA, -TotalB, -ID)%>%distinct(.)
+#tree_dat_wzeros<-left_join(tree_dat_wzeros, plot_dat2) #check warnings about duplicated info 
+#tree_dat_wzeros<-distinct(tree_dat_wzeros)
+
 #put tree cover back in 
-tree_dat2<-dplyr::select(tree_dat, PlotNumber, Species, TotalA, ID)%>%distinct(.)
-tree_dat_wzeros<-left_join(tree_dat_wzeros, tree_dat2)%>%relocate(c(TotalA, ID), .after = Species)
+#tree_dat2<-dplyr::select(tree_dat, PlotNumber, Species, TotalA, TotalB, ID)%>%distinct(.)
+#tree_dat_wzeros<-left_join(tree_dat_wzeros, tree_dat2)%>%relocate(c(TotalA, ID), .after = Species)
 
-#now add in zeroes where total A is NA
-tree_dat_wzeros<-mutate(tree_dat_wzeros, TotalA=if_else(is.na(TotalA), 0, TotalA))
+#gc()
 
-hist(tree_dat_wzeros$TotalA) #very zero inflated 
+#now add in zeroes where cover is NA
+#tree_dat_wzeros<-mutate(tree_dat_wzeros, TotalA=if_else(is.na(TotalA), 0, TotalA))%>%mutate(TotalB=if_else(is.na(TotalB), 0, TotalB))
 
-#check for NAs 
-sort(nacols(tree_dat_wzeros))
+#hist(tree_dat_wzeros$TotalA) #very zero inflated 
+#hist(tree_dat_wzeros$TotalB) #very zero inflated 
 
 #save
-save(tree_dat_wzeros, file="data/tree_data_cleaned_wzeros.Rdata") #too big to push- save on OS #13
+#save(tree_dat_wzeros, file="data/tree_data_cleaned_wzeros.Rdata") #too big to push- save on OS #13
 
 
-#look at most measured spp----- 
+#plot most measured spp----- 
+load(file="data/tree_data_cleaned.Rdata") #only use actual plot data, not imputed zeros 
+
 #Western red Cedar 
-Cw<-subset(tree_dat, Species=='THUJPLI')  #3639 obs
-ggplot(Cw, aes(y=TotalA, x=MAT, color=Site))+
+Cw<-subset(tree_dat, Species=='THUJPLI')  
+ggplot(Cw, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc) 
-#geom_smooth(method='lm', se = F)#+
-#theme(legend.position = 'none')
-#xlim(-2.5, 12) #git rid of outliers 
+  facet_wrap(~GIS_BGC) 
 
 #Western Hemlock
-Hw<-subset(tree_dat, Species=='TSUGHET') #4716 obs
-ggplot(Hw, aes(y=TotalA, x=MAT, color=Site))+
+Hw<-subset(tree_dat, Species=='TSUGHET') 
+ggplot(Hw, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
-#geom_smooth(method='lm', se = F)#+
-#theme(legend.position = 'none')
-# xlim(-2.5, 12) #git rid of outliers 
+  facet_wrap(~GIS_BGC) 
 
 #Engelman Spruce
-Se<-subset(tree_dat, Species=='PICEENE') #4055 obs
-ggplot(Se, aes(y=TotalA, x=MAT, color=Site))+
+Se<-subset(tree_dat, Species=='PICEENE')
+ggplot(Se, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
+  facet_wrap(~GIS_BGC)
+
 
 #Douglas Fir 
-Fd<-subset(tree_dat, Species=="PSEUMEN") #3705 obs
-ggplot(Fd, aes(y=TotalA, x=MAT, color=Site))+
+Fd<-subset(tree_dat, Species=="PSEUMEN") 
+ggplot(Fd, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
+  facet_wrap(~GIS_BGC)
 
 #Subalpine Fir
-Bl<-subset(tree_dat, Species=="ABIELAS") #4060 obs 
-ggplot(Bl, aes(y=TotalA, x=MAT, color=Site))+
+Bl<-subset(tree_dat, Species=="ABIELAS") 
+ggplot(Bl, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
+  facet_wrap(~GIS_BGC)
 
 #Lodgepole Pine 
-Pl<-subset(tree_dat, Species=="PINUCON") #3164 obs
-ggplot(Pl, aes(y=TotalA, x=MAT, color=Site))+
+Pl<-subset(tree_dat, Species=="PINUCON") 
+ggplot(Pl, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
+  facet_wrap(~GIS_BGC)
 
 #Amabalis fir
-Ba<-subset(tree_dat, Species=="ABIEAMA") #1913 obs
-ggplot(Ba, aes(y=TotalA, x=MAT, color=Site))+
+Ba<-subset(tree_dat, Species=="ABIEAMA") 
+ggplot(Ba, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
+  facet_wrap(~GIS_BGC)
 
 #Mountain Hemlock
-Hm<-subset(tree_dat, Species=="TSUGMER") #1337 obs 
-ggplot(Hm, aes(y=TotalA, x=MAT, color=Site))+
+Hm<-subset(tree_dat, Species=="TSUGMER")
+ggplot(Hm, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+ 
-  facet_wrap(~bgc) #why some blank here??
+  facet_wrap(~GIS_BGC) #why some blank here??
 
 #Yellow Cedar
-Yc<-subset(tree_dat, Species=="CALLNOO") #1187 obs
-ggplot(Yc, aes(y=TotalA, x=MAT, color=Site))+
+Yc<-subset(tree_dat, Species=="CALLNOO") 
+ggplot(Yc, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
+  facet_wrap(~GIS_BGC)
 
 #Sitka Spruce 
-Ss<-subset(tree_dat, Species=="PICESIT") #939 obs (>1000 cut off?)
-ggplot(Ss, aes(y=TotalA, x=MAT, color=Site))+
+Ss<-subset(tree_dat, Species=="PICESIT")
+ggplot(Ss, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
+  facet_wrap(~GIS_BGC)
 
 #Trembling Aspen 
-At<-subset(tree_dat, Species=="POPUTRE") #536 obs 
-ggplot(At, aes(y=TotalA, x=MAT, color=Site))+
+At<-subset(tree_dat, Species=="POPUTRE")
+ggplot(At, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
+  facet_wrap(~GIS_BGC)
 
 #Paper birch 
-Ep<-subset(tree_dat, Species=="BETUPAP") #531 obs 
-ggplot(Ep, aes(y=TotalA, x=MAT, color=Site))+
+Ep<-subset(tree_dat, Species=="BETUPAP")
+ggplot(Ep, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
+  facet_wrap(~GIS_BGC)
 
 #Ponderosa Pine
-Py<-subset(tree_dat, Species=="PINUPON") #568 obs
-ggplot(Py, aes(y=TotalA, x=MAT, color=Site))+
+Py<-subset(tree_dat, Species=="PINUPON") 
+ggplot(Py, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc) 
+  facet_wrap(~GIS_BGC) 
 
 #White Spruce 
-Sw<-subset(tree_dat, Species=="PICEGLA")  #397 obs (<500 cut off?)
-ggplot(Sw, aes(y=TotalA, x=MAT, color=Site))+
+Sw<-subset(tree_dat, Species=="PICEGLA")  
+ggplot(Sw, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc) 
+  facet_wrap(~GIS_BGC) 
 
 #Western Larch 
-Lw<-subset(tree_dat, Species=="LARIOCC") #442 obs(<500 cut off?)
-ggplot(Lw, aes(y=TotalA, x=MAT, color=Site))+
+Lw<-subset(tree_dat, Species=="LARIOCC") 
+ggplot(Lw, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc) 
+  facet_wrap(~GIS_BGC) 
 
 #Black Spruce 
-Sb<-subset(tree_dat, Species=="PICEMAR") #313 obs (<500 cut off?)
-ggplot(Sb, aes(y=TotalA, x=MAT, color=Site))+
+Sb<-subset(tree_dat, Species=="PICEMAR") 
+ggplot(Sb, aes(y=TotalA, x=year, color=MoistureRegime_clean))+
   geom_point()+
-  facet_wrap(~bgc)
-
-#uneven sampling across BGCs by species, may need stronger threshold for number obs per BGC per site, species etc
-#currently n=2+ 
-
-#Save top 16 species level datasets
-#save(Cw, Hw, Se, Fd, Bl, Pl, Ba, Hm, Yc, Ss, At, Ep, Py, Sw, Lw, Sb, file="data/tree_spp_data_cleaned.Rdata")
+  facet_wrap(~GIS_BGC)
 
 
 
