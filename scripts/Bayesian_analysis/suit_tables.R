5b838e99c00680a79290fb0cfb58d1080b6dc9ee
Courtney Collins <courtneygrace125@gmail.com>
2026-07-24 09:50:00 -0700

    bring in newest version of suit and edatopic tables and format/save for bayesian modeling

--- scripts/Bayesian_analysis/suit_tables.R
+++ scripts/Bayesian_analysis/suit_tables.R
@@ -33,7 +33,8 @@ library(tidyverse)
 #feas.dat<-read.csv("data/Suitability_v13_19.csv") #v13_19 updated with Ecologist review as of May 15, 2025
 #feas.dat<-read.csv("data/Suitability_v13_22.csv") #v13_22 updated with Craig Delong review & inputed ratings Oct 1, 2025
 #feas.dat<-read.csv("data/Suitability_v13_24.csv") #v13_24 updated with Craig Delong & other review & non reviewed inputed ratings removed Dec 4, 2025
-feas.dat<-read.csv("data/Suitability_v13_25.csv") #v13_25 with Kristi essfwc3, wc7 updates Feb 2026
+#feas.dat<-read.csv("data/Suitability_v13_25.csv") #v13_25 with Kristi essfwc3, wc7 updates Feb 2026
+feas.dat<-read.csv("data/Suitability_v13_30.csv") #most recent April 2026
 
 feas.dat<-subset(feas.dat, spp!='X')#remove any with no species defined 
 
@@ -72,7 +73,86 @@ feas.dat.sub<-mutate(feas.dat.sub, TotalA= replace_na(TotalA, 0), TotalB= replac
 
 feas.dat.sub<-subset(feas.dat.sub, !is.na(newsuit))
 
+#create BGC zone column
+feas.dat.sub$Zone<-NULL #remove old column-incomplete 
+feas.dat.sub<-mutate(feas.dat.sub, zone= case_when(grepl('ICH', ss_nospace)~"ICH",grepl('ESSF', ss_nospace)~"ESSF", grepl('MS', ss_nospace)~"MS", grepl('SWB', ss_nospace)~"SWB", 
+                                                   grepl('SBPS', ss_nospace)~"SBPS", grepl('BAFA', ss_nospace)~"BAFA", grepl('CWH', ss_nospace)~"CWH", grepl('IDF', ss_nospace)~"IDF",
+                                                   grepl('BG', ss_nospace)~"BG", grepl('ESSF', ss_nospace)~"ESSF", grepl('CDF', ss_nospace)~"CDF", grepl('SBS', ss_nospace)~"SBS", 
+                                                   grepl('MH', ss_nospace)~"MH", grepl('CMA', ss_nospace)~"CMA", grepl('PP', ss_nospace)~"PP", grepl('BWBS', ss_nospace)~"BWBS", TRUE~ NA))
+
 save(feas.dat.sub, file="data/feasibility_abundance_data.Rdata") #save 
+
+#QA/QC----
+#calculate average abundances by feas scores
+avgs<-group_by(feas.dat.sub, bgc, zone,ss_nospace, Species, spp, newsuit_ord)%>%
+  summarise(mean_abund_ss=mean(TotalAB, na.rm = T), sd_abund_ss=sd(TotalAB, na.rm = T), nplots_ss=n())
+avgs<-mutate(avgs, sd_abund_ss =replace(sd_abund_ss, is.na(sd_abund_ss), 0))
+
+#filter plot data for quality 
+#throw out anything with only 1 plot or high variability but low coverage (i.e. mean<sd & <5 plots)?
+avgs<-mutate(avgs, sd_abund_ss =replace(sd_abund_ss, is.na(sd_abund_ss), 0))
+avgs<-subset(avgs, nplots_ss>1) 
+avgs$diff<-avgs$mean_abund_ss-avgs$sd_abund_ss
+avgs<-mutate(avgs, remove=if_else(nplots_ss<4 & diff<0, 'Y', 'N')) 
+avgs<-subset(avgs, remove=='N') 
+avgs$remove<-NULL
+avgs$diff<-NULL
+
+#look at prop of site series out of total by spp 
+nss<-select(feas.dat, spp)%>% group_by(spp)%>%summarise(n_site_series=n())
+avgs<-group_by(avgs, spp)%>%mutate(n_ss=n())%>%left_join(., nss)%>%mutate(prop_ss= n_ss/n_site_series)
+select(avgs, spp, prop_ss)%>%distinct(.)
+mean(avgs$prop_ss)
+
+max(avgs$nplots_ss)
+mean(avgs$nplots_ss)
+median(avgs$nplots_ss)
+
+#flag ss to review 
+#check ratings with relative cutoffs
+avgs<-mutate(avgs,review= case_when(newsuit_ord=="5" & mean_abund_ss>0~"Y",
+                                    newsuit_ord=="4" & mean_abund_ss>1~"Y",
+                                    newsuit_ord=="3" & mean_abund_ss> 10~"Y",
+                                    newsuit_ord=="3" & mean_abund_ss< 1~"Y",
+                                    newsuit_ord=="2" & mean_abund_ss> 25~"Y",
+                                    newsuit_ord=="2" & mean_abund_ss< 10~"Y",
+                                    newsuit_ord=="1" & mean_abund_ss<25~"Y",
+                                    TRUE~"N"))
+check<-subset(avgs, review=="Y") #more than half ~3k/5k ratings 
+
+feas.dat.sub<-left_join(feas.dat.sub, avgs)
+feas.dat.validate<-subset(feas.dat.sub, !is.na(review)) #remove anything from modeling dataset that didn't pass quality filter
+
+#don't review parkland subzones, should all be E3/E4
+feas.dat.validate<- mutate(feas.dat.validate, review= ifelse(grepl("p$", bgc), "N", review))
+
+#create edatopes----
+#create a unique variable for the 15 edatopic spaces for modeling 
+feas.dat.validate<- mutate(feas.dat.validate, NutrientRegime_clean=ifelse(NutrientRegime_clean=="F","E", NutrientRegime_clean))# there is no F
+feas.dat.validate<- mutate(feas.dat.validate, MoistureRegime_clean=ifelse(MoistureRegime_clean=="8","7", MoistureRegime_clean)) #lump 8s with 7s to match CCISS
+
+feas.dat.validate$edatope<-paste(feas.dat.validate$NutrientRegime_clean, feas.dat.validate$MoistureRegime_clean, sep="")
+feas.dat.validate<- mutate(feas.dat.validate, edatopex=case_when(edatope=="C3"|edatope=="C4"~"C34",
+                                                                 edatope=="C1"|edatope=="C2"|edatope=="C0"~"C12",
+                                                                 edatope=="C5"|edatope=="C6"|edatope=="C7"~"C56",
+                                                                 edatope=="A3"|edatope=="A4"|edatope=="B3"|edatope=="B4"~"AB34",
+                                                                 edatope=="A0"|edatope=="B0"|edatope=="A1"|edatope=="A2"|edatope=="B1"|edatope=="B2"~"AB12",
+                                                                 edatope=="A7"|edatope=="B7"|edatope=="A5"|edatope=="A6"|edatope=="B5"|edatope=="B6"~"AB56",
+                                                                 edatope=="D3"|edatope=="D4"|edatope=="E3"|edatope=="E4"~"DE34",
+                                                                 edatope=="D0"|edatope=="E0"|edatope=="D1"|edatope=="D2"|edatope=="E1"|edatope=="E2"~"DE12",
+                                                                 edatope=="D7"| edatope=="E7"|edatope=="D5"|edatope=="D6"|edatope=="E5"|edatope=="E6"~"DE56",
+                                                                 TRUE~NA))
+feas.dat.validate$edatope<-as.factor(feas.dat.validate$edatope)
+sort(unique(feas.dat.validate$edatope))
+sort(unique(feas.dat.validate$edatopex))
+
+#remove unused cols 
+names(feas.dat.validate)
+feas.dat.validate<-select(feas.dat.validate, -X, -SuccessionalStatus, -PlotRepresenting, -SiteUnit, -elev_check, -BECSiteUnit)
+
+
+#save output----
+save(feas.dat.validate, file="data/feas_abund_data_validate.Rdata")
 
 #identify things that may not have merged----
 #have suit rating but no supporting plot data 
@@ -89,24 +169,11 @@ BEC_missing_feas$X<-NULL
 #write.csv(BEC_missing_feas, "data/BEC_missing_feas.csv")
 
 #plot by spp ----
-#create BGC zone column
-feas.dat.sub$Zone<-NULL #remove old column-incomplete 
-feas.dat.sub<-mutate(feas.dat.sub, zone= case_when(grepl('ICH', ss_nospace)~"ICH",grepl('ESSF', ss_nospace)~"ESSF", grepl('MS', ss_nospace)~"MS", grepl('SWB', ss_nospace)~"SWB", 
-                                                   grepl('SBPS', ss_nospace)~"SBPS", grepl('BAFA', ss_nospace)~"BAFA", grepl('CWH', ss_nospace)~"CWH", grepl('IDF', ss_nospace)~"IDF",
-                                                   grepl('BG', ss_nospace)~"BG", grepl('ESSF', ss_nospace)~"ESSF", grepl('CDF', ss_nospace)~"CDF", grepl('SBS', ss_nospace)~"SBS", 
-                                                   grepl('MH', ss_nospace)~"MH", grepl('CMA', ss_nospace)~"CMA", grepl('PP', ss_nospace)~"PP", grepl('BWBS', ss_nospace)~"BWBS", TRUE~ NA))
-
-#calculate average abundances by feas scores
-avgs<-group_by(feas.dat.sub, zone, bgc, ss_nospace, Species, spp, newsuit_ord)%>%
-  summarise(mean_abund_ss=mean(TotalAB, na.rm = T), sd_abund_ss=sd(TotalAB, na.rm = T), nplots_ss=n())
-avgs<-mutate(avgs, sd_abund_ss =replace(sd_abund_ss, is.na(sd_abund_ss), 0))
-
-#spp plots ----
 #Ac- 
 ggplot(subset(avgs, spp=="Ac"), aes(x = newsuit_ord, y = mean_abund_ss, alpha=0.5))+
   #geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("At")
+  facet_wrap( ~ zone, scales="free_y") + theme_bw() + theme(legend.position='none') +ggtitle("At")
 #At- 
 ggplot(subset(avgs, spp=="At"), aes(x = newsuit_ord, y = mean_abund_ss, alpha=0.5))+
   #geom_point(position=position_jitterdodge(dodge.width=0.9)) +
@@ -131,7 +198,8 @@ ggplot(subset(avgs, spp=="Bl"), aes(x = newsuit_ord, y = mean_abund_ss, alpha=0.
 ggplot(subset(avgs, spp=="Cw"), aes(x = newsuit_ord, y = mean_abund_ss, alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
-  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Cw")
+  facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Cw")+ 
+  geom_hline(aes(yintercept = 25), lty=2)
 #Ep-
 ggplot(subset(avgs, spp=="Ep"), aes(x = newsuit_ord, y = mean_abund_ss,  alpha=0.5))+
   geom_point(position=position_jitterdodge(dodge.width=0.9)) +
@@ -194,89 +262,9 @@ ggplot(subset(avgs, spp=="Yc"), aes(x = newsuit_ord, y = mean_abund_ss,  alpha=0
   geom_boxplot(fill="white", position=position_dodge(width=0.9), alpha=0.5) +
   facet_wrap( ~ zone) + theme_bw() + theme(legend.position='none') +ggtitle("Yc")
 
-#QA/QC----
-#filter plot data for quality 
-#throw out anything with only 1 plot or high variability but low coverage (i.e. mean<sd & <5 plots)?
-avgs<-mutate(avgs, sd_abund_ss =replace(sd_abund_ss, is.na(sd_abund_ss), 0))
-avgs<-subset(avgs, nplots_ss>1) 
-avgs$diff<-avgs$mean_abund_ss-avgs$sd_abund_ss
-avgs<-mutate(avgs, remove=if_else(nplots_ss<4 & diff<0, 'Y', 'N')) 
-avgs<-subset(avgs, remove=='N') 
-avgs$remove<-NULL
-avgs$diff<-NULL
-
-#look at prop of site series out of total by spp 
-nss<-select(feas.dat, spp)%>% group_by(spp)%>%summarise(n_site_series=n())
-avgs<-group_by(avgs, spp)%>%mutate(n_ss=n())%>%left_join(., nss)%>%mutate(prop_ss= n_ss/n_site_series)
-select(avgs, spp, prop_ss)%>%distinct(.)
-mean(avgs$prop_ss)
-
-max(avgs$nplots_ss)
-mean(avgs$nplots_ss)
-median(avgs$nplots_ss)
-
-#flag ss to review 
-#check ratings with relative cutoffs
-avgs<-mutate(avgs,review= case_when(newsuit_ord=="5" & mean_abund_ss>0~"Y",
-                                    newsuit_ord=="4" & mean_abund_ss>1~"Y",
-                                    newsuit_ord=="3" & mean_abund_ss> 10~"Y",
-                                    newsuit_ord=="3" & mean_abund_ss< 1~"Y",
-                                    newsuit_ord=="2" & mean_abund_ss> 25~"Y",
-                                    newsuit_ord=="2" & mean_abund_ss< 10~"Y",
-                                    newsuit_ord=="1" & mean_abund_ss<25~"Y",
-                                    TRUE~"N"))
-check<-subset(avgs, review=="Y") #more than half ~3k/5k ratings 
-
-feas.dat.sub<-left_join(feas.dat.sub, avgs)
-feas.dat.validate<-subset(feas.dat.sub, !is.na(review)) #remove anything from modeling dataset that didn't pass quality filter
-
-#don't review parkland subzones, should all be E3/E4
-feas.dat.validate<- mutate(feas.dat.validate, review= ifelse(grepl("p$", bgc), "N", review))
 
 
-#create edatopes----
-#create a unique variable for the 15 edatopic spaces for modeling 
-feas.dat.validate<- mutate(feas.dat.validate, NutrientRegime_clean=ifelse(NutrientRegime_clean=="F","E", NutrientRegime_clean))# there is no F
-feas.dat.validate<- mutate(feas.dat.validate, MoistureRegime_clean=ifelse(MoistureRegime_clean=="8","7", MoistureRegime_clean)) #lump 8s with 7s to match CCISS
-
-feas.dat.validate$edatope<-paste(feas.dat.validate$NutrientRegime_clean, feas.dat.validate$MoistureRegime_clean, sep="")
-feas.dat.validate<- mutate(feas.dat.validate, edatopex=case_when(edatope=="C3"|edatope=="C4"~"C34",
-                                                                 edatope=="C1"|edatope=="C2"|edatope=="C0"~"C12",
-                                                                 edatope=="C5"|edatope=="C6"|edatope=="C7"~"C56",
-                                                                 edatope=="A3"|edatope=="A4"|edatope=="B3"|edatope=="B4"~"AB34",
-                                                                 edatope=="A0"|edatope=="B0"|edatope=="A1"|edatope=="A2"|edatope=="B1"|edatope=="B2"~"AB12",
-                                                                 edatope=="A7"|edatope=="B7"|edatope=="A5"|edatope=="A6"|edatope=="B5"|edatope=="B6"~"AB56",
-                                                                 edatope=="D3"|edatope=="D4"|edatope=="E3"|edatope=="E4"~"DE34",
-                                                                 edatope=="D0"|edatope=="E0"|edatope=="D1"|edatope=="D2"|edatope=="E1"|edatope=="E2"~"DE12",
-                                                                 edatope=="D7"| edatope=="E7"|edatope=="D5"|edatope=="D6"|edatope=="E5"|edatope=="E6"~"DE56",
-                                                                 TRUE~NA))
-feas.dat.validate$edatope<-as.factor(feas.dat.validate$edatope)
-sort(unique(feas.dat.validate$edatope))
-sort(unique(feas.dat.validate$edatopex))
-
-#remove unused cols 
-names(feas.dat.validate)
-feas.dat.validate<-select(feas.dat.validate, -X, -SuccessionalStatus, -PlotRepresenting, -SiteUnit, -elev_check, -BECSiteUnit)
-
-#check that each unique plot # for each site series
-check<-select(feas.dat.validate, PlotNumber,ss_nospace)%>%distinct(.)%>%group_by(PlotNumber)%>%
-  mutate(ss_ct=n())%>%filter(ss_ct>1)
-feas.dat.validatex<-subset(feas.dat.validate, PlotNumber %in% check$PlotNumber)
-feas.dat.validate<-subset(feas.dat.validate, !PlotNumber %in% check$PlotNumber)
-
-site_lookup <- feas.dat.validatex %>%
-  distinct(PlotNumber, ss_nospace) %>%
-  group_by(PlotNumber) %>%
-  mutate(suffix = letters[row_number()])
-
-feas.dat.validatex <- feas.dat.validatex %>%
-  left_join(site_lookup, by = c("PlotNumber", "ss_nospace")) %>%
-  mutate(PlotNumber = paste0(PlotNumber, "_", suffix))%>%select(-suffix)
-feas.dat.validate<-rbind(feas.dat.validate, feas.dat.validatex)
-#save output----
-save(feas.dat.validate, file="data/feas_abund_data_validate.Rdata")
-
-
+#OLD----
 #set moist and nutrients as ordinal
 #feas.dat.sub$MoistureRegime_clean<-ordered(feas.dat.sub$MoistureRegime_clean, levels = c(8,7,6,5,4,3,2,1,0))
 #feas.dat.sub$NutrientRegime_clean<-ordered(feas.dat.sub$NutrientRegime_clean, levels = c("F", "E", "D", "C", "B", "A"))
@@ -303,8 +291,22 @@ ggplot(subset(feas.dat.validate,review=="Y"), aes(y = zone))+ geom_bar()
 ggplot(subset(feas.dat.validate,review=="Y"), aes(y = edatope))+ geom_bar()
 ggplot(subset(feas.dat.validate,review=="Y"), aes(y = mod))+ geom_bar()
 
+#USE plot random effect instead 
+#check that each unique plot # for each site series
+#check<-select(feas.dat.validate, PlotNumber,ss_nospace)%>%distinct(.)%>%group_by(PlotNumber)%>%
+#  mutate(ss_ct=n())%>%filter(ss_ct>1)
+#feas.dat.validatex<-subset(feas.dat.validate, PlotNumber %in% check$PlotNumber)
+#feas.dat.validate<-subset(feas.dat.validate, !PlotNumber %in% check$PlotNumber)
 
-  
+#site_lookup <- feas.dat.validatex %>%
+#  distinct(PlotNumber, ss_nospace) %>%
+#  group_by(PlotNumber) %>%
+#  mutate(suffix = letters[row_number()])
+
+#feas.dat.validatex <- feas.dat.validatex %>%
+#  left_join(site_lookup, by = c("PlotNumber", "ss_nospace")) %>%
+#  mutate(PlotNumber = paste0(PlotNumber, "_", suffix))%>%select(-suffix)
+#feas.dat.validate<-rbind(feas.dat.validate, feas.dat.validatex)  
 #pull in climate data ----
 ##select climate variables from BGC model
 #climrVars = c("CMD_sm", "DDsub0_sp", "DD5_sp", "Eref_sm", "Eref_sp", "EXT", 
